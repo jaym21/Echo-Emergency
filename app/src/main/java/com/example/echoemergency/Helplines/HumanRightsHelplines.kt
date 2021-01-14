776afd03f457848a68e28cde3d4e93374bd7b7a6
jaym21 <jaynmistry21@gmail.com>
2021-01-14 18:53:57 +0530

    added redirect to call for helplines

--- app/src/main/java/com/example/echoemergency/Helplines/HumanRightsHelplines.kt
+++ app/src/main/java/com/example/echoemergency/Helplines/HumanRightsHelplines.kt
@@ -1,12 +1,41 @@
 package com.example.echoemergency.Helplines
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.echoemergency.R
+import kotlinx.android.synthetic.main.activity_accident_helplines.*
+import kotlinx.android.synthetic.main.activity_human_rights_helplines.*
 
 class HumanRightsHelplines : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_human_rights_helplines)
+
+        cvHumanHelpline1.setOnClickListener{
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:" + 1091)
+            startActivity(dialIntent)
+        }
+
+        cvHumanHelpline2.setOnClickListener{
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:" + 102)
+            startActivity(dialIntent)
+        }
+
+        cvHumanHelpline3.setOnClickListener{
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:" + 1291)
+            startActivity(dialIntent)
+        }
+
+        cvHumanHelpline4.setOnClickListener{
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:" + 1098)
+            startActivity(dialIntent)
+        }
+
     }
 }
--- app/src/main/java/com/example/echoemergency/Helplines/MedicalHelplines.kt
+++ app/src/main/java/com/example/echoemergency/Helplines/MedicalHelplines.kt
@@ -1,12 +1,40 @@
 package com.example.echoemergency.Helplines
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.echoemergency.R
+import kotlinx.android.synthetic.main.activity_medical_helplines.*
+import kotlinx.android.synthetic.main.activity_police_helplines.*
 
 class MedicalHelplines : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_medical_helplines)
+
+        cvMedicalHelpline1.setOnClickListener{
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:" + 102)
+            startActivity(dialIntent)
+        }
+
+        cvMedicalHelpline2.setOnClickListener{
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:" + 9540161344)
+            startActivity(dialIntent)
+        }
+
+        cvMedicalHelpline3.setOnClickListener{
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:" + 1097)
+            startActivity(dialIntent)
+        }
+
+        cvMedicalHelpline4.setOnClickListener{
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:" + 1066)
+            startActivity(dialIntent)
+        }
     }
 }
