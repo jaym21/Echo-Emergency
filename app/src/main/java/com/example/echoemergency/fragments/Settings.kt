e1edac2a0cedf28febaa2b04a03a7b25665df9c0
jaym21 <jaynmistry21@gmail.com>
2021-02-19 14:05:37 +0530

    added hindi strings

--- app/src/main/java/com/example/echoemergency/fragments/Settings.kt
+++ app/src/main/java/com/example/echoemergency/fragments/Settings.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,6 +23,8 @@ class Settings : Fragment(), INumberRVAdapter {
     private lateinit var binding: FragmentSettingsBinding
 
     private lateinit var viewModel: NumberViewModel
+    private lateinit var spinner: Spinner
+    private lateinit var arrayAdapter: ArrayAdapter<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,6 +38,11 @@ class Settings : Fragment(), INumberRVAdapter {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        //initializing spinner
+        spinner = binding.spLanguage
+
+        arrayAdapter = ArrayAdapter(this, )
 
         //initializing viewModel by casting this as MainActivity so that we have access to the viewModel created in MainActivity
         viewModel = (activity as MainActivity).viewModel
